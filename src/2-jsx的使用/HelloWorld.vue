b57c462094a548de5258859a42783e3035a1abaf
chenfengbin <1838272147@qq.com>
2021-12-01 21:16:45 +0800

    jsx use

---
+++ src/2-jsx的使用/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+export default {
+  render() {
+    return (
+      <div>
+        <h2>hello world</h2>
+        {this.$slots.default ? this.$slots.default({name:'chen'}) : <span>哈哈哈哈</span>}
+      </div>
+    );
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './1-render函数的使用/App.vue'
+import App from './2-jsx的使用/App.vue'
 
 createApp(App).mount('#app')
