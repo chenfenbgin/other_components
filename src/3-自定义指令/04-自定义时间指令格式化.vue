9e289dbebe94aa372f72a4aa92410ac167d0fc7d
chenfengbin <1838272147@qq.com>
2021-12-02 20:19:53 +0800

    v-format-time

---
+++ src/3-自定义指令/04-自定义时间指令格式化.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!-- 以后如果需要对时间进行格式化，只需要加上这个指令就行了，v-format-time -->
+    <h2 v-format-time="'YYYY/MM/DD'">{{timestamp}}</h2>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  setup() {
+    const timestamp = 1624452193;
+
+    
+
+    return{
+      timestamp
+    }
+  },
+};
+</script>
+
+<style scoped>
+</style>
